4d8c2296d186582f841bc17c418a81d47c7241e5
aleixcarretero2@gmail.com <aleixcarretero2@gmail.com>
2026-05-28 14:04:37 +0200

    fix leaflet nextjs client component

---
+++ app/dashboard/MapClient.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default function MapClient({ pickups }: any) {
+  return (
+    <div style={{ height: 400, marginBottom: 30 }}>
+
+      <MapContainer
+        center={[41.98, 2.82] as any}
+        zoom={12}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {pickups?.map((p: any) =>
+          p.latitude && p.longitude ? (
+            <Marker
+              key={p.id}
+              position={[p.latitude, p.longitude] as any}
+            >
+              <Popup>
+                <b>{p.client_name}</b><br />
+                {p.status}
+              </Popup>
+            </Marker>
+          ) : null
+        )}
+
+      </MapContainer>
+
+    </div>
+  )
+}
