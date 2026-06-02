3efc2cf38c6bd331b375f83728b002bbc03ead55
aleixcarretero2@gmail.com <aleixcarretero2@gmail.com>
2026-06-02 09:47:40 +0200

    Arreglat sw.js i afegit id/scope al manifest d'operaris

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,15 @@
-self.addEventListener('fetch', () => {});
+// Aquest esdeveniment permet que el navegador consideri l'app com a PWA
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+  });
+  
+  // Aquest FETCH handler és el correcte: 
+  // Si no vols fer estratègies offline complexes, 
+  // simplement deixa que les peticions passin per la xarxa.
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+  });
